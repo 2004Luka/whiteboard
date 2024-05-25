9c9a037df7f733b1a8d41e8201c6dc95072d3ed3
2004Luka <luka.chakhunashvili1@gmail.com>
2024-05-25 21:31:25 +0400

    update

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-let express = require('express')
-let app = express();
-let httpserver= require('http').createServer(app);
-let io= require('socket.io')(httpserver);
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpserver = http.createServer(app);
+const io = require('socket.io')(httpserver);
 
+let connections = [];
+
+io.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(`${socket.id} has connected`);
+
+    socket.on('draw', (data) => {
+        connections.forEach(con => {
+            if (con.id !== socket.id) {
+                con.emit('ondraw', { x: data.x, y: data.y });
+            }
+        });
+    });
+
+    socket.on("down",(data)=>{
+        connections.forEach((con)=> {
+            if(con.id !== socket.id){
+                con.emit("ondown",{x: data.x ,y: data.y});
+            }
+        })
+    })
+
+    socket.on('disconnect', () => {
+        connections = connections.filter(con => con.id !== socket.id);
+        console.log(`${socket.id} has disconnected`);
+    });
+});
 
 app.use(express.static('public'));
 
-
-let PORT = process.env.PORT || 8080
-app.listen(PORT,()=>console.log(`server started ib port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+httpserver.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
--- public/script.js
+++ public/script.js
@@ -3,6 +3,8 @@ let canva =document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
 
+var io =io.connect("http://localhost:8080/")
+
 let ctx = canvas.getContext('2d');
 
 let x;
@@ -12,18 +14,31 @@ let mouseDown=false;
 
 window.onmousedown =(e)=>{
     ctx.moveTo(x,y);
+    io.emit("down",{x,y});    
     mouseDown=true;
-
 }
 
 
 window.onmouseup=(e)=>{
     mouseDown=false;
 }
+
+
+io.on('ondraw',({x,y})=>{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+})
+io.on("ondown",({x,y})=>{
+    ctx.moveTo(x,y);
+})
+
 window.onmousemove =(e)=>{
     x=e.clientX;
     y=e.clientY;
+
     if(mouseDown){
+    
+        io.emit('draw',{x,y});
         ctx.lineTo(x,y);
         ctx.stroke();
     }
